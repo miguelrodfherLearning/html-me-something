8d4be8f73cf70640d0f666f91c989c5b13ca0452
miguelrodfher <miguelrodfher@gmail.com>
2020-04-24 22:39:18 -0400

    Completed Assignment 4

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('a[href*=#main]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
